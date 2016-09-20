60d20b792e376c09424117ea002f646d200985f4
runnedrun <none>
2016-09-20 09:49:27 -0700

    Classadoo autocommit

---
+++ challenge2.js
@@ -0,0 +1,17 @@
+var songLyrics = "Like baby, baby, baby no oh. Thought you’d always be mine, mine";
+
+// challenge 2
+// make a LIST containing each word in the song, then add a new element
+// to the page for each word in the song.
+//
+//Hint:
+// newDiv = $("<div></div>")
+// $("body").append(newDiv)
+
+// challenge 3
+// Now make an object containing the number of times each word appears in the song
+
+// Hint: 
+// var wordCounts = {}
+// wordCounts[songWord] = wordCounts[songWord] + 1
+// console.log(wordCounts)
